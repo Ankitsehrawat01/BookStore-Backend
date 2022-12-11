a2fdff6430956573dd87de2d8ec28f209b182f70
Ankitsehrawat01 <sehrawata328@gmail.com>
2022-12-12 02:50:39 +0530

    [Ankit Kumar]: Admin Login API Created

--- Book Store Database/AdminTableData.sql
+++ Book Store Database/AdminTableData.sql
@@ -1,1 +1,3 @@
 INSERT INTO AdminTable values('Admin', 'Admin@gmail.com','Admin@123','7988660207')
+
+INSERT INTO AdminTable values('Admin','ankitfundooapp@gmail.com','Admin@123','7988660207')
--- Book Store Database/Stored Procedure/Admin/AdminLogin.sql
+++ Book Store Database/Stored Procedure/Admin/AdminLogin.sql
@@ -1,6 +1,6 @@
 CREATE OR ALTER PROCEDURE Sp_AdminLogin
-@EmailId VARCHAR(100), @Password VARCHAR (100)
-AS
-BEGIN
-SELECT Email_Id,Password FROM AdminTable WHERE Email_Id= @EmailId AND Password=@Password
-END
+@Email_Id VARCHAR(50), @Password VARCHAR (100)
+as
+--SQL Statement
+SELECT Email_Id,Password FROM AdminTable WHERE Email_Id= @Email_Id AND Password=@Password
+go
